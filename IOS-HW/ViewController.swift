c0c532ba69d7e697ec766b31c634f1da15182eb3
Student11 <student11@Imac3s-Mac-mini.local>
2024-06-26 17:43:36 +0300

    init project

---
+++ IOS-HW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS-HW
+//
+//  Created by Student11 on 26/06/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
